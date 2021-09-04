5bfbd05ff24096b44c16cba55106bf3c2e05e54e
pinkishincoloragain <smb1103@gmail.com>
2021-09-05 03:55:32 +0900

    making IG input form

--- src/views/Interest.js
+++ src/views/Interest.js
@@ -1,7 +1,28 @@
-import React from "react";
+import { React, useState } from "react";
 import "styles/Interest.scss";
+import ShowLetter from "views/showLetter";
 
 function Interest() {
-  return <div className="recipient">Interest</div>;
+  const [verified, setVerified] = useState(false);
+  return (
+    <div className="recipient">
+      {verified ? (
+        <ShowLetter />
+      ) : (
+        <div>
+          Instagram ID :
+          <form className="inputForm">
+            <input
+              autoFocus="true"
+              className="inputText"
+              type="text"
+              name="name"
+            />
+            <input className="submitBtn" type="submit" value="Submit" />
+          </form>
+        </div>
+      )}
+    </div>
+  );
 }
 export default Interest;
---
+++ src/views/showLetter.js
@@ -0,0 +1,8 @@
+import { React, useState } from "react";
+import "styles/Interest.scss";
+
+function ShowLetter() {
+  const [verified, setVerified] = useState(false);
+  return <div>Message</div>;
+}
+export default ShowLetter;
